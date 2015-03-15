c8bf0b3e283d675bf265c7a935bb2b6a9885dbe2
Sabina Smajlaj <sabinasmajlaj@gmail.com>
2015-03-15 11:37:58 -0400

    tried to read in image, but this method isn't working

--- cs4735/sabinadev/assign2/src/ColorMatching.java
+++ cs4735/sabinadev/assign2/src/ColorMatching.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 import org.opencv.core.Core;
@@ -22,70 +26,167 @@ public class ColorMatching
     * color histogram. 
     */
 	//public void generateHistogram(){
-	public void run(){
+	public void run() throws IOException{
 		//TODO
 		// Read in the image
-		Mat src;  
-		src = Highgui.imread(getClass().getResource("Images/i15.jpg").getPath());
-		//Imgproc.cvtColor(src, src, Imgproc.COLOR_RGB2GRAY);
-		Highgui.imwrite("histoimage.jpg", src);
-		List<Mat> rgb_planes = new ArrayList<Mat>(); 
-		Core.split(src, rgb_planes);
+		//Mat src;  
+		//src = Highgui.imread(getClass().getResource("Images/i15.jpg").getPath());
+		RGBPixel[][] pixels= new RGBPixel[89][60];
+		pixels = readSImage("i15.ppm"); 
+		for(int i = 0; i< 10; i++){
+			for(int j = 0; j<10; j++){
+				//System.out.println(pixels[i][j].redVal); 
+			}
+		}
 		
-		MatOfInt histSize = new MatOfInt(512);
-	    final MatOfFloat histRange = new MatOfFloat(0, 256);
-	    boolean accumulate = false;
-		Mat r_hist = new Mat();
-		Mat g_hist = new Mat();
-		Mat b_hist = new Mat(); 
-		
-		List<Mat> r_plane = new ArrayList<Mat>();
-		r_plane.add(rgb_planes.get(0)); 
-		List<Mat> g_plane = new ArrayList<Mat>();
-		g_plane.add(rgb_planes.get(1)); 
-		List<Mat> b_plane = new ArrayList<Mat>();
-		b_plane.add(rgb_planes.get(2)); 
-		
-		Imgproc.calcHist(r_plane, new MatOfInt(0), new Mat(), r_hist, histSize, histRange, accumulate);
-		Imgproc.calcHist(g_plane, new MatOfInt(0), new Mat(), g_hist, histSize, histRange, accumulate);
-		Imgproc.calcHist(b_plane, new MatOfInt(0), new Mat(), b_hist, histSize, histRange, accumulate);
-
-		int hist_w = 89;
-		int hist_h = 60;
-		long bin_w = Math.round((double) hist_w / 512); 
-		
-		Mat histImage = new Mat(hist_h, hist_w, CvType.CV_8UC1);
-		
-		Core.normalize(r_hist, r_hist, 3, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
-		Core.normalize(g_hist, g_hist, 3, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
-		Core.normalize(b_hist, b_hist, 3, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
-		
-		for (int i = 1; i < 256; i++) {
-			Core.line(
-	                histImage,
-	                new org.opencv.core.Point( i, histImage.rows() ),
-	                new org.opencv.core.Point( i, histImage.rows()-Math.round( r_hist.get(i,0)[0] )) ,
-	                new Scalar( 255, 255, 255),
-	                1, 8, 0 );
-
-			Core.line(
-	                histImage,
-	                new org.opencv.core.Point( i, histImage.rows() ),
-	                new org.opencv.core.Point( i, histImage.rows()-Math.round( g_hist.get(i,0)[0] )) ,
-	                new Scalar( 255, 255, 255),
-	                1, 8, 0 );
-
-			Core.line(
-	                histImage,
-	                new org.opencv.core.Point( i, histImage.rows() ),
-	                new org.opencv.core.Point( i, histImage.rows()-Math.round( b_hist.get(i,0)[0] )) ,
-	                new Scalar( 255, 255, 255),
-	                1, 8, 0 );
-
-	    }
-		Highgui.imwrite("histogram.jpg", histImage);
 	}
 	
+	public static RGBPixel[][] readSImage(String fName) throws IOException
+	{
+		System.out.println("this"); 
+		File theFile = new File(fName);
+		System.out.println("file"); 
+		Scanner scan = new Scanner(theFile);
+		System.out.println("scanner"); 
+		int i=0, j=0;
+		int tokenCounter = 0;
+		int cols = 0;
+		int rows = 0;
+		StringTokenizer tokens = null;
+		
+		outloop: 
+		while (scan.hasNextLine())
+		{
+			System.out.println("after"); 
+			String aLine = scan.nextLine();
+			if (aLine.charAt(0) == '#'){
+				System.out.println("saw #"); 
+				continue;
+			}
+			// process the line:
+			tokens = new StringTokenizer(aLine, " ");
+			while (tokens.hasMoreTokens())
+			{
+				tokenCounter++;
+				String aToken = tokens.nextToken();
+				if (tokenCounter == 1) 
+				{
+					// expect that aToken is "P6"
+					if (!aToken.equals("P6")) System.out.println("Expected P6 magic number, got " + aToken);
+				}
+				if (tokenCounter == 2)
+				{
+					// expect that aToken is the number of columns
+					cols = Integer.parseInt(aToken);
+				}
+				if (tokenCounter == 3)
+				{
+					// expect that aToken is the number of rows
+					rows = Integer.parseInt(aToken);
+				}
+				if (tokenCounter == 4)
+				{
+					// expect that aToken is the maximum value
+					// ignore
+					break outloop;
+				}//inner if
+			}//inner while	
+		}//outer while
+
+		RGBPixel outImage[][] = new RGBPixel[rows][cols];
+
+		// still worry if more tokens in previously read line
+
+		int channelCount = 0;
+		int redVal = 0, blueVal = 0, greenVal = 0;
+		while (scan.hasNextLine())
+		{
+			String aLine = scan.nextLine();
+			if (aLine.charAt(0) == '#')
+				continue;
+
+			// process the line:
+			tokens = new StringTokenizer(aLine, " ");
+			while (tokens.hasMoreTokens())
+			{
+				String aToken = tokens.nextToken();
+				if (channelCount == 0) 
+				{
+					redVal = Integer.parseInt(aToken);
+					channelCount++;
+				}
+				else
+				if (channelCount == 1) 
+				{
+					// expect that aToken is the number of columns
+					greenVal = Integer.parseInt(aToken);
+					channelCount++;
+				}
+				else
+				if (channelCount == 2) 
+				{
+					// expect that aToken is the number of rows
+					blueVal = Integer.parseInt(aToken);
+					outImage[i][j] = new RGBPixel(redVal, greenVal, blueVal);
+					j++;
+					if (j >= cols)
+					{
+						i++;
+						j = 0;
+					}
+					channelCount = 0;
+		
+				}//inner if
+			}//inner while		
+		}//outer while
+/*
+		// possible that there's still some tokens on the line already read
+		while (tokens.hasMoreTokens())
+		{
+			int redVal = Integer.parseInt(tokens.nextToken());
+			int greenVal = Integer.parseInt(tokens.nextToken());
+			int blueVal = Integer.parseInt(tokens.nextToken());
+			outImage[i][j] = new RGBPixel(redVal, greenVal, blueVal);
+			j++;
+			if (j >= cols)
+			{
+				i++;
+				j = 0;
+			}
+		}
+
+		while (scan.hasNextLine())
+		{
+			String line = scan.nextLine();
+			StringTokenizer tokens2 = new StringTokenizer(line, " ");
+			while (tokens2.hasMoreTokens())
+			{
+				int redVal = Integer.parseInt(tokens2.nextToken());
+				int greenVal = Integer.parseInt(tokens2.nextToken());
+				int blueVal = Integer.parseInt(tokens2.nextToken());
+				outImage[i][j] = new RGBPixel(redVal, greenVal, blueVal);
+				j++;
+				if (j >= cols)
+				{
+					i++;
+					j = 0;
+				}
+			}
+		}
+
+*/
+
+		return outImage;
+	}
+
+	
+	public void readImage(String filename){
+		//Read in image line by line
+		//starting from line 5, break the line into 89*3 sections, 
+		//with each section corresponding to a byte
+		
+		//
+	}
 	/*
 	 * Takes in two histograms and using the normalized L1
 	 * comparison, compares the histograms and determines 
@@ -127,7 +228,7 @@ public class ColorMatching
 		//TODO
 	}
 	
-	public static void main( String[] args )
+	public static void main( String[] args ) throws IOException
    {
       System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
       ColorMatching colorMatching = new ColorMatching(); 
---
+++ cs4735/sabinadev/assign2/src/RGBPixel.java
@@ -0,0 +1,12 @@
+
+public class RGBPixel {
+	int redVal;
+	int blueVal;
+	int greenVal;
+	
+	public RGBPixel(int red, int blue, int green){
+		redVal = red;
+		blueVal = blue;
+		greenVal = green;
+	}
+}
